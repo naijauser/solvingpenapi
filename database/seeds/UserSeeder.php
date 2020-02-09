a54990cc08236c22a9b7bd88a5f7524e3ef15280
naijauser <unix1gl@gmail.com>
2020-02-09 04:25:05 +0100

    Separated name column to firstName and lastName respectively. Also implemented faker for seeding users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -5,32 +5,46 @@ use App\User;
 
 class UserSeeder extends Seeder {
 
-
     public function run () {
 
         /**
          * Insert the dummy data into the users table
          */
 
-        User::create([
-            'name' => 'nnamdi',
-            'email' => 'a@b.com',
-            'password' => bcrypt('000000'),
+         // Clear the database
+        //  User::truncate();
 
-        ]);
+         // Use the factory to create a Faker\Generator instance.
+        $faker = \Faker\Factory::create('en_NG');
 
-        User::create([
-            'name' => 'tope',
-            'email' => 't@p.com',
-            'password' => bcrypt('000000'),
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'firstName' => $faker->firstName,
+                'lastName' => $faker->lastName,
+                'email' => $faker->email,
+                'password' => bcrypt('000000'),
+            ]);
+        }
 
-        ]);
+        // User::create([
+        //     'name' => 'nnamdi',
+        //     'email' => 'a@b.com',
+        //     'password' => bcrypt('000000'),
 
-        User::create([
-            'name' => 'novados',
-            'email' => 'n@s.com',
-            'password' => bcrypt('000000'),
+        // ]);
 
-        ]);
+        // User::create([
+        //     'name' => 'tope',
+        //     'email' => 't@p.com',
+        //     'password' => bcrypt('000000'),
+
+        // ]);
+
+        // User::create([
+        //     'name' => 'novados',
+        //     'email' => 'n@s.com',
+        //     'password' => bcrypt('000000'),
+
+        // ]);
     }
 }
