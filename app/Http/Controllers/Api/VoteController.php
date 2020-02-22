45caf8ae3a9ccb74f736197ae06b114ce4dcee8a
naijauser <unix1gl@gmail.com>
2020-02-22 22:20:52 +0100

    included the getVote, addVote and deleteVote methods to the voteContoller

--- app/Http/Controllers/Api/VoteController.php
+++ app/Http/Controllers/Api/VoteController.php
@@ -5,7 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class VoteController extends Controller
-{
-    //
+class VoteController extends Controller {
+    private $statusOk = 'OK';
+
+    public function getVotes (Request $request) {
+
+    }
+
+    public function addVote (Request $request) {
+
+    }
+
+    public function deleteVote (Request $request) {
+
+    }
 }
