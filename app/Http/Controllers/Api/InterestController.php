87844cf5f490f518347bd19a1fd767d75e6aa0e1
naijauser <unix1gl@gmail.com>
2020-02-22 21:31:35 +0100

    included the Interest, Question and QuestionSpecialization Controllers with the initial methods

---
+++ app/Http/Controllers/Api/InterestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InterestController extends Controller {
+    private $statusOk = 'OK';
+
+    public function addInterest (Request $request) {
+
+    }
+
+    public function deleteInterest (Request $request, $interestId) {
+
+    }
+
+    public function updateInterest (Request $request, $interestId) {
+
+    }
+}
---
+++ app/Http/Controllers/Api/QuestionSpecializationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QuestionSpecializationController extends Controller {
+    private $statusOk = 'OK';
+
+    public function addRelationship () {
+
+    }
+
+    public function removeRelationship () {
+
+    }
+}
